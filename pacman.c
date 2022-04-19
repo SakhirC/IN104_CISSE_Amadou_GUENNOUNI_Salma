4deeb10dce1d16214b258eeacb6d085da1335ba3
SakhirC <amadou.cisse@ensta-paris.fr>
2022-04-19 15:30:09 +0200

    nouveau

---
+++ pacman.c
@@ -0,0 +1,28 @@
+#define height 20
+#define width 20
+#define number 4
+
+
+
+struct coor{
+    int x;
+    int y;
+}
+
+struct PacMan{
+    struct coor coord;
+    int nx;
+    int ny;
+    int lives;
+    int food;
+    bool chase;
+}
+
+struct ghost{
+    struct coor coord;
+    int nx;
+    int ny;
+    bool chase;
+}
+
+
